3e4810d5ae273622b24c4c72966f7cedffd6a3b0
Emerik <emerik.aji@nishyda.tech>
2023-09-25 16:31:13 +0200

    initial commit

---
+++ page/page.js
@@ -0,0 +1,23 @@
+let button = document.getElementById("checker");
+button.addEventListener("click", check);
+
+function check() {
+    button.innerHTML = "⟳"
+    button.disabled = true;
+    button.classList.remove("hover");
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, async tabs => {
+        let url = tabs[0].url;
+        const domain = (new URL(url)).hostname.replace('www.','');
+        const res = await fetch("http://52.17.79.12:32770/domains/check/" + domain);
+        console.log(res);
+        const resJson = await res.json();
+        const belongs = resJson.Belongs;
+        if (belongs) {
+            button.innerHTML = "Oui! ✔";
+            button.classList.add("good");
+        } else {
+            button.innerHTML = "Non! ✗";
+            button.classList.add("bad");
+        }
+    });
+}
