cf9fc4abbeb949e6df840ef40f57ed4cd731b61e
Emerik <emerik.aji@nishyda.tech>
2024-01-16 14:32:46 +0100

    match API Spec

--- page/page.js
+++ page/page.js
@@ -8,7 +8,7 @@ function check() {
     browser.tabs.query({currentWindow: true, active: true}).then(async (tabs) => {
         let url = tabs[0].url;
         const domain = (new URL(url)).hostname.replace('www.','');
-        const res = await fetch("https://supervssl.api.niwanet.net/domains/check/" + domain);
+        const res = await fetch("https://supervssl.api.niwanet.net/sites/" + domain + "?check=true");
         console.log(res);
         const resJson = await res.json();
         const belongs = resJson.Belongs;
